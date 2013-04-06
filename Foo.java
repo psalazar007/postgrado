f19c2915089bfedc2605f65b9e33a10f3d0578a8
root <root@Ariel-PC.(none)>
2013-04-05 21:34:11 -0400

    Segundo commit de la Rama 1

--- Foo.java
+++ Foo.java
@@ -1,10 +1,7 @@
 package util;
 public class Foo {
-   private String firstName;
-   private String lastName;
-
-   public String getFirstName() {
-     return firstName;
+  
+rn firstName;
    }
    public void setFirstName(String firstName) {
       this.firstName = firstName;
